714639d9d557b52cee49567754c5c29282ed42c7
Joel Winterton <joel.winterton@outlook.com>
2025-01-15 14:57:31 +0000

    Refactored

--- truncated_levy.py
+++ truncated_levy.py
@@ -2,22 +2,27 @@ import scipy.stats as ss
 import numpy as np
 from scipy.special import gamma, gammainc, exp1
 
-def UpperGamma(a,x):
+
+def upper_gamma(a, x):
+
     return gamma(a) * (1 - gammainc(a, x))
 
-def ExpIntegralE(n,z):
+
+def exp_integral_e(n, z):
     """
     Returns Mathematica equivalent for exponential n integral.
     :param n:
     :param z:
     :return:
     """
-    return z**(n-1)*UpperGamma(1-n, z)
+    return z ** (n - 1) * upper_gamma(1 - n, z)
+
 
 class TruncatedLevy(ss.rv_continuous):
     """
     Distribution for an exponentially weighted Levy distribution.
     """
+
     def __init__(self, beta=0.75, x0=1.5, k=80):
         super().__init__(a=0, b=np.inf)
         self.beta = beta
@@ -31,7 +36,7 @@ class TruncatedLevy(ss.rv_continuous):
         Calculate the normalising constant for the PDF.
         :return:
         """
-        return np.exp(self.x0 / self.k) * self.k ** (1 - self.beta) * UpperGamma(1 - self.beta, self.x0 / self.k, )
+        return np.exp(self.x0 / self.k) * self.k ** (1 - self.beta) * upper_gamma(1 - self.beta, self.x0 / self.k, )
 
     def _get_support(self, *args, **kwargs):
         """
